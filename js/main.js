007d49a5757ad43748b5eea53f89250c9bfcfbd3
Graphic <graphic@SOD-SJH-516-35MT.local>
2015-09-25 13:27:28 -0400

    dynamic mouseover functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-$('.blue').addClass('light');
+// $('.blue')
+// 	.addClass('light')
+// 	.css('background-color', 'orange')
+// 	.text('This is a test!');
+
+
+
+
+// OVER/OUT
+// When I hover over a panel, light it up
+$('.panel')
+	.on('mouseover', function() { // OVER
+		
+		console.log("OVER");
+		
+		// Add the light class
+		$(this).addClass('light');
+	})
+	.on('mouseout', function() { // OUT
+		
+		console.log("OUT");
+		
+		// Remove the light class
+		$(this).removeClass('light');
+	
+	});
+
+
+
+
+
+
+
